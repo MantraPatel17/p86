5141a6ad67d93f2dabd56fd290970d2be3e5ee10
MantraPatel17 <71125259+MantraPatel17@users.noreply.github.com>
2021-07-27 23:11:19 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+ apiKey: "AIzaSyCqWr17791cn03WuSNrqYQfRbUbi3u9lTw",
+ authDomain: "storytellingapp-4286f.firebaseapp.com",
+ databaseURL: "https://storytellingapp-4286f-default-rtdb.firebaseio.com",
+ projectId: "storytellingapp-4286f",
+ storageBucket: "storytellingapp-4286f.appspot.com",
+ messagingSenderId: "663286883407",
+ appId: "1:663286883407:web:08935741b44b0cee26363a"
+};
